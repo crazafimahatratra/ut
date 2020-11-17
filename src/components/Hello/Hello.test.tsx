df57f9826eaa5ded10f2fddcd4daf5313636a4bc
Christophe <christophe@MacBook-Pro-de-MBP.local>
2020-11-17 16:45:08 +0300

    Hello

---
+++ src/components/Hello/Hello.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Hello from "./Hello";
+
+let container:HTMLDivElement | null = null;
+beforeEach(() => {
+    // met en place un élément DOM comme cible de rendu
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // nettoie en sortie de test
+    if(container) {
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+    }
+});
+
+it("s’affiche avec ou sans nom", () => {
+    act(() => {
+        render(<Hello />, container);
+    });
+    expect(container?.textContent).toBe("Salut, étranger");
+
+    act(() => {
+        render(<Hello name="Jenny" />, container);
+    });
+    expect(container?.textContent).toBe("Bonjour, Jenny !");
+
+    act(() => {
+        render(<Hello name="Margaret" />, container);
+    });
+    expect(container?.textContent).toBe("Bonjour, Margaret !");
+});
